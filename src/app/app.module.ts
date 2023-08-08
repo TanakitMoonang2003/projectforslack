86be935976b9e3b7b5912865e5f7f126fa91705a
tanakit2003 <tanakit2003moonang@gmail.com>
2023-08-08 16:58:19 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    AboutusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
